e1b0fb749a58ded0c906d60db5b5250411f157ad
Davidmdawe <mdawedavid@gmail.com>
2019-08-01 16:50:45 +0200

    david

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
-
+import {davidComponent} from './david/david.component'
 
 const routes: Routes = [
   {path : 'home', component: HomeComponent},
+  {path : 'david', component: davidComponent},
 ];
 
 @NgModule({
---
+++ src/app/david/david.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-david',
+  templateUrl: './david.component.html',
+  styleUrls: ['./david.component.scss']
+})
+export class DavidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
